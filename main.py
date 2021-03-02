63902bf37d6e5dc610711785cfae9343591a8af8
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2021-03-02 14:28:47 +0700

    Add file

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 from fastapi import FastAPI, File, UploadFile
+import PIL
+from PIL import Image
+import botnoi.resnet50 as rn
+import shutil
+from botnoi import cv
+import os
+import pickle
+import numpy as np
+from sklearn.svm import LinearSVC
+from io import BytesIO
 app = FastAPI()
+
+# output function
+modFile = 'mymod.mod'
+mod = pickle.load(open(modFile,'rb'))
+
+
+@app.post("/image")
+async def image(image: UploadFile = File(...)):
+    #file#Image.open(image.file)
+    with open("file.png",'wb') as f:
+        shutil.copyfileobj(image.file, f)
+    feat = rn.extract_feature("file.png")
+    res = mod.predict([feat])[0]
+    return {"class":res}
 
 
 @app.get("/")
@@ -10,3 +34,8 @@ def read_root():
 @app.get("/text/{text}")
 def read_word(text: str):
     return {"word": text}
+
+
+if __name__ == '__main__':
+   import uvicorn
+   uvicorn.run(app, host="0.0.0.0", port=5000, debug=True) 
