9a053046499201f05c44da0854038397165c0a33
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2021-03-02 14:30:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -38,4 +38,4 @@ def read_word(text: str):
 
 if __name__ == '__main__':
    import uvicorn
-   uvicorn.run(app, host="0.0.0.0", port=5000, debug=True) 
+   uvicorn.run(app, host="0.0.0.0", port=80, debug=True) 
